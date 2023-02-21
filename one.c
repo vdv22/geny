1003b6b2e5a44e3fa56556df1ea2a3569bcdf77b
vdv22 <vdv2@mail.ru>
2023-02-22 00:00:26 +0700

    add txt

--- one.c
+++ one.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 typedef uint8_t u08;
 u08 data[8]={10,15,32,14,80,92,56,89};
-struct book
-{
-	  char name[20];
-	  u08  size;
-	  u08 hild; 
-};
+char str[30];
 
-typedef struct book Tbook;
-
+ void unwhite(const char* str)
+ {
+	for(int i ; i<strlen(str) ; i++)
+	{
+		
+		if (str[i]!=' ')
+		{
+			printf("%c",str[i]);
+			
+		}
+		
+	}
+	 
+ }
+ 
 
 int main(int argc, char **argv)
 {
- Tbook oneBook;
- Tbook* PoneBook=NULL;
- PoneBook=&oneBook;
- PoneBook->size=156;
- PoneBook->hild=89;
- 
- printf("%d  %d",PoneBook->size,PoneBook->hild);
- 
- 
 
+  gets(str);
+  unwhite(str);
+  
 	return 0;
 }
 
